a93a9151dad857510a744774ff46966717776e70
ker33 <kewuuu33@gmail.com>
2026-05-03 14:02:28 +0800

    Delete llava/eval/run_attention_heatmap.sh

--- llava/eval/run_attention_heatmap.sh
+++
@@ -1,5 +0,0 @@
-python ./llava/eval/draw_attention_heatmap.py \
-    --model-path ./checkpoints/llava-v1.5-7b-official \
-    --image-path ./playground/data/coco/val2014/COCO_val2014_000000000923.jpg \
-    --question "Is there a train in the image?" \
-    --output-path "heatmap_train.jpg"
